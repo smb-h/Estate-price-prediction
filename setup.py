12481adbcd84c2411fc48c4721ed80da55361030
SMB H <smbhop@pm.me>
2023-01-27 19:36:45 +0330

    :construction: architecture(structure): update project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hpp",
+    packages=find_packages(),
+    version="0.1.0",
+    description="House price prediction",
+    author="SMBH",
+    license="MIT",
+)
